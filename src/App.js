2099fcbb9fed630ba7e992328fb1d2b4340173d9
midori1727 <midori.takaichi@iths.se>
2022-01-23 00:56:34 +0100

    created repo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import FrontPage from './components/FrontPage';
+import PokemonLogo from './pokemon-logo.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img className="logo" src={PokemonLogo} alt='pokemon-logo' /> 
+		<FrontPage />
       </header>
     </div>
   );
